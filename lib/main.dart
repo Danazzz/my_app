db02ac2f1a913f7b0e8f31f5535997e3da390acf
Danazzz <artaputra95@gmail.com>
2021-04-04 22:55:52 +0800

    convert todo implement createstate menjadi expresion body

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // ignore: todo
-    // TODO: implement createState
-    return _MyAppState();
-  }
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
